ed8f7c69c697391e71963052828c1c993d3163ab
piotrkardys <piotrk@mobilewebpro.pl>
2016-04-04 12:03:31 +0200

    Lesson 3

--- App.js
+++ App.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 class App extends React.Component {                   //creating component, class has a field called state
   render() {
-    return <h1>Hello</h1>                             //2 ways to return HTML element
+    //return <h1>Hello</h1>                           //2 ways to return HTML element
     //return React.createElement('h1', null, 'Hi');
+
+    //return <div><h1>Hi</h1><h2>hello</h2></div>     //we can "return" more than 1 element when we include them into larger element (e.g. div)
+                                                      //in that way we still returning 1 element, but with more elements inside
+    return (                                          //if we want to format element which we are returning, we need to take it into the brackets
+      <div>
+        <h1>Hello</h1>
+        <h1>My name is Piotrek</h1>
+      </div>
+    )
   }
 }
 
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 "04.04.2016 - 8:50 - start of the project" 
 "04.04.2016 - 10:30 - Lesson1: react and babel configuration, creating files - index.html, App.js, main.js, webpack.config.js" 
 "04.04.2016 - 11:30 - Lesson2: update file App.js" 
+"04.04.2016 - 12:00 - Lesson3: update file App.js" 
