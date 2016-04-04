41ae83bd6f187cf589d9228e2bb45b9e9255f12f
piotrkardys <piotrk@mobilewebpro.pl>
2016-04-04 11:36:57 +0200

    Lesson 2

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-class App extends React.Component {
+
+class App extends React.Component {                   //creating component, class has a field called state
   render() {
-    return <div>Hello</div>
+    return <h1>Hello</h1>                             //2 ways to return HTML element
+    //return React.createElement('h1', null, 'Hi');
   }
 }
+
+//const App = () => <h1>HelloFunction</h1>            //this is also right way, but function doesn't have state field
 
 export default App
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 "REACT FUNDAMENTALS" 
 "04.04.2016 - 8:50 - start of the project" 
-"04.04.2016 - 10:30 - react and babel configuration, creating files: index.html, App.js, main.js, webpack.config.js" 
+"04.04.2016 - 10:30 - Lesson1: react and babel configuration, creating files - index.html, App.js, main.js, webpack.config.js" 
+"04.04.2016 - 11:30 - Lesson2: update file App.js" 
